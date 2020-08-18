a2ccf67b1e67d5b1fe3c3020e99b88b520ba9fbd
Felipe <felipelee22@gmail.com>
2020-08-18 20:53:39 -0300

    Enviando arquivos

---
+++ script.js
@@ -0,0 +1,16 @@
+//Determinar numero  máximo de colunas e linhas
+//Coletar dados de coluna e linhas
+//criar matriz com dados aleatórios
+//Restringir a quantidade de numeros 1(bombas)
+//Criar campo com o numero de celulas informado
+//detectar clique
+//Calcular minas adjacentes
+var linhas = document.getElementById("linha").value
+var colunas = document.getElementById("coluna").value
+
+
+
+function start(){}
+function clicked(){
+    //document.getElementById("") = visited
+}
