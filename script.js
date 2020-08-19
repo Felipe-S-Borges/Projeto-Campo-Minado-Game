62f30dd5ccfc40de706201d93070326f44f65a1e
Felipe <felipelee22@gmail.com>
2020-08-19 09:26:08 -0300

    Coletar dados de Col e Lin

--- script.js
+++ script.js
@@ -1,15 +1,30 @@
-//Determinar numero  máximo de colunas e linhas
-//Coletar dados de coluna e linhas
-//criar matriz com dados aleatórios
-//Restringir a quantidade de numeros 1(bombas)
-//Criar campo com o numero de celulas informado
-//detectar clique
-//Calcular minas adjacentes
-var linhas = document.getElementById("linha").value
-var colunas = document.getElementById("coluna").value
+//[x]Determinar numero  máximo de colunas e linhas
+//[x]Coletar dados de coluna e linhas
+//[]criar matriz com dados aleatórios
+//[]Restringir a quantidade de numeros 1(bombas)
+//[]Criar célula
+//[]Criar campo com o numero de celulas informado(Matriz de célula)
+//[]detectar clique
+//[]Calcular minas adjacentes
+
+var linhas = 0
+var colunas = 0 
+
+function inserirLinha(){
+
+    linhas = document.getElementById("linha")
+    linhas = linhas.value
+
+}
+
+function inserirColuna(){
+
+    colunas = document.getElementById("coluna")
+    colunas = colunas.value
+}
 
 
-
+function criarCelula(){}
 function start(){}
 function clicked(){
     //document.getElementById("") = visited
